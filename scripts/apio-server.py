40c5418f74c80842f16fb86eed6aee5cc48fa497
jcarolinares <jcarolin@everis.com>
2019-07-16 13:20:16 +0200

    Basic APIO Ros Service working

--- scripts/apio-server.py
+++ scripts/apio-server.py
@@ -4,14 +4,26 @@
 import rospy
 from ros_apio.srv import *
 
+import subprocess
+
 def handle_ros_apio(req):
     print ("Returning: {}".format(req.command))
+    if (req.command=="init"):
+        subprocess.call("apio init -y -p circuits/ -b icezum", shell=True)
+    elif (req.command=="verify"):
+        subprocess.call("apio verify -p ./circuits/", shell=True)
+    elif (req.command=="build"):
+        subprocess.call("apio build -p ./circuits/", shell=True)
+    elif (req.command=="upload"):
+        subprocess.call("apio upload -p ./circuits/", shell=True)
+    else:
+        rospy.logerr("Apio command not valid")
     return RosApioResponse(req.command)
 
 def ros_apio():
     rospy.init_node('ros_apio_server')
     s = rospy.Service('ros_apio_server', RosApio, handle_ros_apio)
-    print "Ros Apio Server Ready"
+    rospy.loginfo("Ros Apio Server Ready")
     rospy.spin()
 
 if __name__ == "__main__":
